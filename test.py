d5b2da425ca1189ca68d95f6588ac42995b0c0d2
Jonathan <jonwool@users.noreply.github.com>
2021-05-14 18:36:23 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+path = 'C:\\Users\\AC617LA\\OneDrive - EY\\Desktop\\Army Contract\\RP_GFEBS_Automation\\data'
+
+# 041 data path
+path_041 = path + '\\041_data'
+os.chdir(path_041)
+
+# create empty variables for list of files / dataframe
+files_041 = []
+df = pd.DataFrame()
